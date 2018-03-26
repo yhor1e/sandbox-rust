94c7bfa42bd82b953cc97f3ceb564534a3a217f3
yhorie <yuk.horie@gmail.com>
2018-03-26 23:16:03 +0900

    Finish primitive_types2

---
+++ rustlings/primitive_types2/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let my_first_initial = 'C';
+    if my_first_initial.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if my_first_initial.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Nether alphavetic nor numeric!");
+    }
+
+    let your_character = '1';
+
+    if your_character.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if your_character.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Nether alphavetic nor numeric!");
+    }
+}
