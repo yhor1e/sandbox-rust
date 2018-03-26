a4c5578df69ed52c48bc2e8370188f1d626b17b6
yhorie <yuk.horie@gmail.com>
2018-03-26 23:21:14 +0900

    Finish primitive_type3

---
+++ rustlings/primitive_types3/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a = ["foo", "bar", "baz"];
+
+    if a.len() >= 100 {
+        println!("Wow, that's big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
