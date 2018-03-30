297fefa8a6cb7ef63f635462a91fffb95e526047
yhorie <yuk.horie@gmail.com>
2018-03-31 07:53:21 +0900

    Finish move_semantics2

--- rustlings/move_semantics1/src/main.rs
+++ rustlings/move_semantics1/src/main.rs
@@ -3,6 +3,12 @@ fn main() {
 
     let mut vec1 = fill_vec(vec0);
 
+    // -----------------------------------------------------
+    // 1. Make another, separate version of the data that's in `vec0` and pass that
+    // to `fill_vec` instead.
+    // ----------------------------------------------------
+    //let mut vec1 = fill_vec(vec0.clone());
+
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
@@ -12,6 +18,13 @@ fn main() {
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
+
+    // -----------------------------------------------------
+    // 2. Make `fill_vec` borrow its argument instead of taking ownership of it,
+    // and then copy the data within the function in order to return an owned
+    // `Vec<i32>`
+    // ----------------------------------------------------
+    //let mut vec: Vec<i32> = vec;
 
     vec.push(22);
     vec.push(44);
